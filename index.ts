41d490a3b39b04cb51a5b948d1209f1a94bd01fa
ntonx <ntonx9@gmail.com>
2020-01-28 23:36:36 -0600

    seccion4

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import router from './routes/router';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 
-const server = new Server();
+const server = Server.instance;
 
 // BodyParser
 server.app.use( bodyParser.urlencoded({ extended: true }) );
@@ -20,6 +20,6 @@ server.app.use('/', router );
 
 
 server.start( () => {
-    console.log(`Servidor corriendo en el puerto ${ server.port }`);
-});
+     console.log(`Servidor corriendo en el puerto ${ server.port }`);
+ });
 
